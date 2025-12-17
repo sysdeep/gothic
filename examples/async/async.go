4b053f2edf07b0e62f2127261a53e2568b1fa3cc
Igor Nefedkin <igor.nefedkin@kaspersky.com>
2025-12-17 23:18:44 +0300

    update async example

--- examples/async/async.go
+++ examples/async/async.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/sysdeep/gothic/gothic"
@@ -10,6 +11,11 @@ func main() {
 	ir := gothic.NewInterpreter(`
 		pack [ttk::progressbar .bar1] -padx 20 -pady 20
 		pack [ttk::progressbar .bar2] -padx 20 -pady 20
+		pack [tk::label .label -text lbl] -padx 20 -pady 20
+
+		pack [ttk::button .exit_btn -text Exit -command exit ] -padx 20 -pady 20
+
+		bind . <Control-q> { exit }
 	`)
 
 	go func() {
@@ -36,6 +42,20 @@ func main() {
 			i += inc
 			time.Sleep(1e8)
 			ir.Eval(`.bar2 configure -value %{}`, i)
+		}
+	}()
+
+	go func() {
+		i := 0
+		inc := -1
+
+		for {
+			if i > 99 || i < 1 {
+				inc = -inc
+			}
+			i += inc
+			time.Sleep(time.Second * 1)
+			ir.Eval(fmt.Sprintf(".label configure -text %d", i))
 		}
 	}()
 
